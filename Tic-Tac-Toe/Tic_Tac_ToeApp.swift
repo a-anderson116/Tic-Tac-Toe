43c0bb15dd31698235be3a7caebd01fe02749d42
Alex Anderson <andersonaj116@gmail.com>
2022-11-14 09:40:05 -0600

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Alex Anderson on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
